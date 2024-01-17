623512ab96d1c17621685c5a524f727b659ccac8
Gwendoline Jacobs <gwendolinejacobs1@gmail.com>
2024-01-17 16:31:26 +0100

    end of the day

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,8 +2,10 @@
 using System.Text;
 using API.Controllers;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -17,14 +19,25 @@ public class AccountController: BaseController
 
     [HttpPost("register")] // POST : api/account/register
     
-    public async Task<ActionResult<AppUser>> Register(string username, string password) {
-        using var hmac = new HMACSHA3_512();
+    public async Task<ActionResult<AppUser>> Register(Registerdto registerdto) {
+
+        if(await UserExists(registerdto.Username)) return BadRequest("Username is taken :(");
+        using var hmac = new HMACSHA512();
         var user = new AppUser
         {
-             UserName = username;
-             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+             UserName = registerdto.Username.ToLower(),
+             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerdto.Password)),
+             PasswordSalt = hmac.Key,
 
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return user;
+
+
+    }        
+    private async Task<bool> UserExists(string username){
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
-
-    }
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0492a585e60dfcfa4abc92d00d8676abad3b7307")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b05f36a183f60d00072126363f7b78f9ebff8deb")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
